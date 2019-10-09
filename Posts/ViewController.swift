8641c3ea8bfe794bf9092d138212a1e98df5d3f7
Santiago Pelaez <santirua@gmail.com>
2019-10-09 15:32:19 -0500

    Initial Commit

---
+++ Posts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Posts
+//
+//  Created by Santiago Pelaez Rua on 10/9/19.
+//  Copyright © 2019 Santiago Pelaez Rua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
