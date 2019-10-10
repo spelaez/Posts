afbfb6df5df0563feb4bde116721f86e0055befc
Santiago Pelaez <santirua@gmail.com>
2019-10-10 18:53:39 -0500

    routing funcs to posts list and data passing

--- Posts/Scenes/PostDetails/PostDetailsRouter.swift
+++ Posts/Scenes/PostDetails/PostDetailsRouter.swift
@@ -13,7 +13,7 @@
 import UIKit
 
 @objc protocol PostDetailsRoutingLogic {
-    //func routeToSomewhere(segue: UIStoryboardSegue?)
+    func routeToPostsList()
 }
 
 protocol PostDetailsDataPassing {
@@ -25,33 +25,24 @@ class PostDetailsRouter: NSObject, PostDetailsRoutingLogic, PostDetailsDataPassi
     var dataStore: PostDetailsDataStore?
     
     // MARK: Routing
-    
-    //func routeToSomewhere(segue: UIStoryboardSegue?)
-    //{
-    //  if let segue = segue {
-    //    let destinationVC = segue.destination as! SomewhereViewController
-    //    var destinationDS = destinationVC.router!.dataStore!
-    //    passDataToSomewhere(source: dataStore!, destination: &destinationDS)
-    //  } else {
-    //    let storyboard = UIStoryboard(name: "Main", bundle: nil)
-    //    let destinationVC = storyboard.instantiateViewController(withIdentifier: "SomewhereViewController") as! SomewhereViewController
-    //    var destinationDS = destinationVC.router!.dataStore!
-    //    passDataToSomewhere(source: dataStore!, destination: &destinationDS)
-    //    navigateToSomewhere(source: viewController!, destination: destinationVC)
-    //  }
-    //}
+    func routeToPostsList() {
+        if let dataStore = dataStore,
+            let viewController = viewController,
+            let destinationVC = viewController.navigationController?.viewControllers.first as? PostsListViewController,
+            var destinationDS = destinationVC.router?.dataStore {
+
+            passDataToPostsList(source: dataStore, destination: &destinationDS)
+            navigateToPostsList(source: viewController, destination: destinationVC)
+        }
+    }
     
     // MARK: Navigation
-    
-    //func navigateToSomewhere(source: PostDetailsViewController, destination: SomewhereViewController)
-    //{
-    //  source.show(destination, sender: nil)
-    //}
+    func navigateToPostsList(source: PostDetailsViewController, destination: PostsListViewController) {
+        source.navigationController?.popViewController(animated: true)
+    }
     
     // MARK: Passing data
-    
-    //func passDataToSomewhere(source: PostDetailsDataStore, destination: inout SomewhereDataStore)
-    //{
-    //  destination.name = source.name
-    //}
+    func passDataToPostsList(source: PostDetailsDataStore, destination: inout PostsListDataStore) {
+        destination.updatePost(post: source.post)
+    }
 }
