fc126a9899e981de54852be75e0274ecca3e2024
Santiago Pelaez <santirua@gmail.com>
2019-10-11 00:06:36 -0500

    adopt realm on PostsListWorker for cache and crud operations

--- Posts/Scenes/PostsList/PostsListWorker.swift
+++ Posts/Scenes/PostsList/PostsListWorker.swift
@@ -12,21 +12,41 @@
 
 import UIKit
 import Alamofire
+import RealmSwift
 
 class PostsListWorker {
     private var postsUrl = "https://jsonplaceholder.typicode.com/posts"
+    private var realm: Realm?
+
+    init() {
+        do {
+            self.realm = try Realm()
+        } catch {
+            print("couldn't instantiate realm")
+            print(error)
+        }
+    }
 
     /**
      fetch posts from network or cache when available
      - returns: An array of posts
      */
     func fetchPosts(completionHandler: @escaping (([Post]) -> ())){
-        Alamofire.request(postsUrl).responseJSON { dataResponse in
+        let cachedPosts = fetchPostsFromCache()
+
+        if cachedPosts.isEmpty {
+            Alamofire.request(postsUrl).responseJSON { dataResponse in
                 if let data = dataResponse.data {
                     let decoder = JSONDecoder()
 
                     do {
                         let posts = try decoder.decode([Post].self, from: data)
+                        for post in posts {
+                            try self.realm?.write {
+                                self.realm?.add(post)
+                            }
+                        }
+
                         completionHandler(posts)
                     } catch {
                         print("error trying to decode response")
@@ -34,25 +54,81 @@ class PostsListWorker {
                     }
                 }
             }
+        } else {
+            completionHandler(cachedPosts)
         }
+    }
+
+    private func fetchPostsFromCache() -> [Post] {
+        return Array(realm!.objects(Post.self))
+    }
 
     func filter(posts: [Post], by filter: PostsList.FilterPosts.Filter) -> [Post] {
         switch filter {
         case .all:
             return posts
         case .favorites:
-            return posts.filter { $0.isFavorite }
+            let filteredPosts = realm?.objects(Post.self).filter { $0.isFavorite }
+
+            return Array(filteredPosts!)
         }
     }
 
     /**
-     delete a single post
+     delete a single post from realm
      - parameter id: id for the post to be deleted as Int
      - returns: An array of posts without the deleted post
      */
-    func deletePost(id: Int, on posts: inout [Post]) {
+    func deletePost(id: Int, on posts: inout [Post]) -> Int? {
         if let index = posts.firstIndex(where: { $0.id == id }) {
-            posts.remove(at: index)
+            let removedPost = posts.remove(at: index)
+
+            do {
+                try realm?.write {
+                    realm?.delete(removedPost)
+                }
+
+                return index
+            } catch {
+                print(error)
+            }
+        }
+
+        return nil
+    }
+
+    /**
+     deletes all given posts from realm
+     */
+    func deleteAllPosts(posts: [Post]) {
+        do {
+
+            try realm?.write {
+                realm?.delete(posts)
+            }
+        } catch {
+            print(error)
+        }
+    }
+
+    func updatePost(post: Post) {
+        do {
+            try realm?.write {
+                realm?.add(post, update: .modified)
+            }
+        } catch {
+            print("couldn't update post")
+            print(error)
+        }
+    }
+
+    func markUnreadPost(post: Post) {
+        do {
+            try realm?.write {
+                post.isUnread = true
+            }
+        } catch {
+            print("coudldn't mark post as unread")
         }
     }
 }
